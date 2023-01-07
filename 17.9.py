6887b5b77256f29c146e82e8c1229d0eeee4c4cf
ademarrus <119788923+ademarrus@users.noreply.github.com>
2023-01-07 19:27:13 +0300

    Update 17.9.py

--- 17.9.py
+++ 17.9.py
@@ -26,30 +26,32 @@ def merge(left, right):
         all_num.append(right[j])
         j += 1
     return all_num
-print(merge_sort(array))
+print(merge_sort(num_array))
 
 #  БЛОК ЗАПРОСА И ПРОВЕРКИ ЛЮБОГО ЧИСЛА, ЗАДАННОГО ПОЛЬЗОВАТЕЛЕМ
 while True:
     try:
         number = int(input("Введите любое число в диапазоне от 1 до 999: "))
-        if number < 0 or number > 999:
-            raise Exception
-        break
+   #     if number < 0 or number > 999:
+    #        raise Exception
+     #   break
     except ValueError:
         print("Вы ввели неверный формат данных. Пожалуйста, введите число")
-    except Exception:
-        print("Вы ввели число, не воздящее в требуемый диапазон!")
+   # except Exception:
+    #    print("Вы ввели число, не входящее в требуемый диапазон!")
 
-def binary_search(array, element, left, right):
+# БЛОК АЛГОРИТМА БИНАРНОГО ПОИСКА
+
+def binary_search(num_array, number, left, right):
     if left > right:  # если левая граница превысила правую,
         return False  # значит элемент отсутствует
     middle = (right + left) // 2  # находимо середину
-    if array[middle] == element:  # если элемент в середине,
+    if num_array[middle] == number:  # если элемент в середине,
         return middle  # возвращаем этот индекс
-    elif element < array[middle]:  # если элемент меньше элемента в середине
+    elif number < num_array[middle]:  # если элемент меньше элемента в середине
         # рекурсивно ищем в левой половине
-        return binary_search(array, element, left, middle - 1)
+        return binary_search(num_array, number, left, middle - 1)
     else:  # иначе в правой
-        return binary_search(array, element, middle + 1, right)
+        return binary_search(num_array, number, middle + 1, right)
 
-print(binary_search(array, element, 0, len(array)))
+print(binary_search(num_array, number, 0, len(num_array)))
